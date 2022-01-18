480fd834159d2b19e761e9b87e2fb1d85d544509
lerm <61450666+luutrinhlam@users.noreply.github.com>
2022-01-18 15:31:39 +0700

    Update in_addr_gen.v with stride

--- src/hdl/in_addr_gen.v
+++ src/hdl/in_addr_gen.v
@@ -22,6 +22,7 @@
 
 module in_addr_gen(
 clk,rst,
+stride,
 width,
 channel,
 addr_inc,
@@ -33,38 +34,39 @@ row_cnt,
 channel_cnt
     );
     parameter BRAM_ADDR_BIT = 32;
-    
+
     input clk, rst, addr_inc;
     input [11:0]width, channel;
-    
-    
+    input [1:0] stride;
+
+
     output reg [BRAM_ADDR_BIT-1:0]addr_r0, addr_r1, addr_r2;
     output reg [11:0]entry_cnt;
     output reg [11:0]row_cnt;
     output reg [11:0]channel_cnt;
-    
+
     wire entry_end, row_end, channel_end;
 /////////////////////////////////////////////////////////////////////////////
-    assign entry_end = entry_cnt==width-1;
-    assign row_end = row_cnt==width-3;
+    assign entry_end = width < entry_cnt+ 3 + stride;
+    assign row_end = width < row_cnt+ 3 + stride;
     assign channel_end = channel_cnt==channel-1;
 /////////////////////////////////////////////////////////////////////////////
-    
+
     // entry counter
     always@(posedge clk) begin
         if(rst) begin
             entry_cnt <= 0;
         end
         else if(addr_inc)begin
-            if(entry_cnt==width-1) begin
+            if(width < entry_cnt+ 3 + stride) begin
                 entry_cnt <= 0;
             end
             else begin
-                entry_cnt <= entry_cnt+1;
+                entry_cnt <= entry_cnt+stride;
             end
         end
     end
-    
+
     // row counter
     always@(posedge clk) begin
         if(rst) begin
@@ -72,16 +74,16 @@ channel_cnt
         end
         else if(addr_inc)begin
             if(entry_end) begin
-                if(row_cnt==width-3) begin
+                if(width < row_cnt+ 3 + stride) begin
                     row_cnt <= 0;
                 end
                 else begin
-                    row_cnt <= row_cnt+1;
+                    row_cnt <= row_cnt+stride;
                 end
             end
         end
     end
-    
+
     // channel counter
     always@(posedge clk) begin
         if(rst) begin
@@ -98,7 +100,7 @@ channel_cnt
             end
         end
     end
-    
+
     always@(posedge clk) begin
         if(rst) begin
             addr_r0<=0;
@@ -112,16 +114,21 @@ channel_cnt
                 addr_r2<= {width, 1'b0};
             end
             else if(row_end & entry_end) begin
-                addr_r0<=addr_r0+{width, 1'b0}+1;
-                addr_r1<=addr_r1+{width, 1'b0}+1;
-                addr_r2<=addr_r2+{width, 1'b0}+1;
+                addr_r0<=addr_r0 -entry_cnt + {width, 1'b0} + (width << (width - row_cnt-3));
+                addr_r1<=addr_r1 -entry_cnt + {width, 1'b0} + (width << (width - row_cnt-3));
+                addr_r2<=addr_r2 -entry_cnt + {width, 1'b0} + (width << (width - row_cnt-3));
+            end
+             else if(entry_end) begin
+                addr_r0<= addr_r0 - entry_cnt + (width<<(stride -1));
+                addr_r1<= addr_r1 - entry_cnt + (width<<(stride -1));
+                addr_r2<= addr_r2 - entry_cnt + (width<<(stride -1));
             end
             else begin
-                addr_r0<=addr_r0+1;
-                addr_r1<=addr_r1+1;
-                addr_r2<=addr_r2+1;
+                addr_r0<=addr_r0+stride;
+                addr_r1<=addr_r1+stride;
+                addr_r2<=addr_r2+stride;
             end
         end
     end
-    
+
 endmodule
