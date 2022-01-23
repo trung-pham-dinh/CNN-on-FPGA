84527b30d19353e9ccb922974d685fa66670c56c
lerm <61450666+luutrinhlam@users.noreply.github.com>
2022-01-23 19:31:26 +0700

    Update weight_init.py

--- src/program/bram_weight_init/weight_init.py
+++ src/program/bram_weight_init/weight_init.py
@@ -1,17 +1,16 @@
 filename = "bram_weight_init_"
+
 def append_hex(a, b,size):
     sizeof_b = 8*size
-
-
-
     return (a << sizeof_b) | b
-def bram_weight_init(kernel, width, height, channel):
+    
+def bram_weight_init(weight, kernel, width, height, channel):
 	numberOfEntries = width * height
 
 	quarterOfChannel = channel/4
 	quarterOfKernel = kernel/4
 
-	value = 0x01
+	value = 0
 	file_index = [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15]
 	for i in file_index:
 	    filename_temp = filename + str(i) + ".coe"
@@ -20,9 +19,8 @@ def bram_weight_init(kernel, width, height, channel):
 	    count = 0
 	    temp = 0x0
 	    for j in range (int(numberOfEntries*quarterOfKernel*quarterOfChannel)):
-	        temp = append_hex(value,temp,count)
+	        temp = append_hex(weight[value],temp,count)
 	        value = value + 1
-	        if (value>= 256): value = 0;
 	        count = count + 1
 	        if(count == 4):
 	            f.write(str(hex(temp)[2:])+ " ")
@@ -33,4 +31,4 @@ def bram_weight_init(kernel, width, height, channel):
 	    f.write(";")
 	    f.close()
 
-bram_weight_init(4,3,3,8) #parameters: kernel, width, height, channel
+bram_weight_init([i+1 for i in range(4*3*3*4)],4,3,3,4) #parameters: weight, kernel, width, height, channel
